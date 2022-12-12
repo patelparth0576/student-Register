24dbf36e3897e8fe340f5c81872a011a8ea49d7b
patelparth0576 <120379679+patelparth0576@users.noreply.github.com>
2022-12-12 16:47:39 +0530

    Add files via upload

---
+++ simple.py
@@ -0,0 +1,7 @@
+def personal_details():
+    username, firstname, lastname, pincode, email = "Patel","Parth","Null Innvotaion",394210,"Patelparth2018"
+    address = "Bangalore, Karnataka, India"
+    print("username: {}\firstname: {}\nlastname: {}\npincode:  {}\nemail {}".format(username, firstname, lastname, pincode, email))
+
+personal_details()
+
